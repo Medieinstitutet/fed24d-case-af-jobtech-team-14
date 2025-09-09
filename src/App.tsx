d9c94046bc090e54356587def774af17df7bbb62
JanisSeibutis <janis.seibutis@gmail.com>
2025-09-09 20:35:14 +0200

    chore: install axios, styled-componets and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return <></>;
+  return (
+    <h1 className="text-3xl font-bold underline text-amber-500">
+      Hello world! <br></br>Tailwind works if you see amber!
+    </h1>
+  )
 }
 
-export default App;
+export default App
