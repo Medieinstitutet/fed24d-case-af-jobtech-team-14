43d1d4959c0c2921fefecc61bf270f39570fc5a4
JanisSeibutis <janis.seibutis@gmail.com>
2025-09-10 10:57:29 +0200

    chore: install af designsystem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { DigiButton } from '@digi/arbetsformedlingen-react'
+import { ButtonVariation } from '@digi/arbetsformedlingen' // observera att import av enums sker via @digi/arbetsformedlingen
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-amber-500">
-      Hello world! <br></br>Tailwind works if you see amber!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-amber-500">
+        Hello world! <br></br>Tailwind works if you see amber!
+      </h1>
+      <DigiButton
+        afVariation={ButtonVariation.PRIMARY}
+        onAfOnClick={() => console.log('Hallå världen!')}
+      >
+        Skicka
+      </DigiButton>
+    </>
   )
 }
 
