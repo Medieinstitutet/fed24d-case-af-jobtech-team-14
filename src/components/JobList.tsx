790387c497ffc5be86f12a75073d8ffff7bdb171
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-09-11 09:36:22 +0200

    feat: add DigiButton with DigiIconHeart in JobList card

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -6,7 +6,7 @@ import {
 } from '@digi/arbetsformedlingen'
 import {
   DigiButton,
-  DigiIconCopy,
+  DigiIconHeart,
   DigiLayoutBlock,
   DigiTypography,
 } from '@digi/arbetsformedlingen-react'
@@ -14,43 +14,37 @@ import {
 export const JobList = () => {
   return (
     <>
-      <section className="jobb-list">
+      <section className="job-list-section">
         <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
-          <div className="jobb-container">
-            <div className="jobb-text">
+          <div className="job-list-container">
+            <div className="job-list-text">
               <DigiTypography afVariation={TypographyVariation.SMALL}>
                 <h2>Handläggare med intresse för Programming</h2>
-                <DigiTypography afVariation={TypographyVariation.SMALL}>
-                  <p>Socialstyrelsen - Stockholm</p>
-                </DigiTypography>
-                <DigiTypography afVariation={TypographyVariation.SMALL}>
-                  <p>Statistiker</p>
-                </DigiTypography>
-                <DigiTypography afVariation={TypographyVariation.SMALL}>
-                  <p>Publicerad 2 september kl 16.19</p>
-                </DigiTypography>
               </DigiTypography>
+              <DigiTypography afVariation={TypographyVariation.SMALL}>
+                <p>Socialstyrelsen - Stockholm</p>
+              </DigiTypography>
+              <DigiTypography afVariation={TypographyVariation.SMALL}>
+                <p>Statistiker</p>
+              </DigiTypography>
+              <DigiTypography afVariation={TypographyVariation.SMALL}>
+                <p>Publicerad 2 september kl 16.19</p>
+              </DigiTypography>
+              {/* </DigiTypography> */}
             </div>
-            <DigiButton
-              afSize={ButtonSize.MEDIUM}
-              afVariation={ButtonVariation.FUNCTION}
-              afFullWidth={false}
-            >
-              <DigiIconCopy slot="icon" />
-              En knapp
-            </DigiButton>
+            <div className="jobb-actions">
+              <DigiButton
+                afSize={ButtonSize.MEDIUM}
+                afVariation={ButtonVariation.FUNCTION}
+                afFullWidth={false}
+              >
+                <DigiIconHeart slot="icon" />
+                Spara
+              </DigiButton>
+            </div>
           </div>
         </DigiLayoutBlock>
       </section>
-
-      {/* <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
-        <DigiTypography afVariation={TypographyVariation.SMALL}>
-            <h2>Handläggare med intresse för Programming</h2>
-            <p>Socialstyrelsen - Stockholm</p>
-            <p>Statistiker</p>
-            <p>Publicerad 2 september kl 16.19</p>
-        </DigiTypography>
-        </DigiLayoutBlock> */}
     </>
   )
 }
