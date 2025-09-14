790a4d9cac4994f162218ee28ba75fd5fdfe43fd
JanisSeibutis <janis.seibutis@gmail.com>
2025-09-14 11:20:37 +0200

    feat: add types for occupations

---
+++ src/models/occupationModels.ts
@@ -0,0 +1,13 @@
+export type OccupationField = {
+  'taxonomy/id': 'string'
+  'taxonomy/type': 'string'
+  'taxonomy/preferred-label': 'string'
+}
+
+export type OccupationGroup = {
+  'taxonomy/type': string
+  'taxonomy/definition': string
+  'taxonomy/id': string
+  'taxonomy/ssyk-code-2012': string
+  'taxonomy/preferred-label': string
+}
