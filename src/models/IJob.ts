cd359e5e0faa3df7d2e77973680e9406c5010b67
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-09-15 15:03:06 +0200

    refactor: update IJob-types
    Co-Authored-By: Alexandra <183548137+axandranathalie@users.noreply.github.com>

--- src/models/IJob.ts
+++ src/models/IJob.ts
@@ -4,4 +4,12 @@ export type IJob = {
   location: string
   role: string
   publication_date: string
+  description?: string
+  application_email?: string
+  application_url?: string
+  extent?: string
+  duration?: string
+  employment_type?: string
+  application_deadline?: string
+  salary_description?: string
 }
